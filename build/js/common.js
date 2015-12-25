0c6b33beecbbb0ccb35bb13a276bd78d1d8cbd96
Sergey <veremey4uk@gmail.com>
2015-12-25 23:39:52 +0200

    slider

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,85 @@
 $(document).ready(function() {
 
-	// go js
+	// check select
+	function select() {
+		$(".js-select").each(function(){
+			var select_list = $(this).parent().find(".js-select-list");
+			var text = select_list.find("li").first().text();
+			$(this).find(".js-select-text").text(text);
+			$(this).click(function(event){
+				if ($(this).hasClass("is-active")) {
+				    $(this).removeClass("is-active");
+				    select_list.slideUp("fast");
+				}
+				else {
+				    $(".js-select").removeClass("is-active");
+				    $(".js-select-list").hide();
+				    select_list.slideDown("fast");
+				    $(this).addClass("is-active");
+				}
+				event.stopPropagation();
+			});
+			select_list.find("li").click(function(event) {
+				var id = $(this).attr("data-id");
+				var text = $(this).text();
+				$(this).parent().parent().find(".js-select-text").text(text);
+				$(this).parent().parent().find(".js-select-input").val(id);
+				$(this).parent().hide();
+				$(this).parents(".js-select").removeClass("is-active");
+				event.stopPropagation();
+			});
+		});
+	}
+	select();
 
-});
+	$('.js-select').click(function(event){
+	    event.stopPropagation();
+	});
+
+
+	/* Скрипт для рейтинга */
+
+	$('.stars').each(function(){
+
+		var rating = $(this);
+		var rating_input = rating.parents('form').find('input[name="rating"]');
+		var stars = $('.star > span', rating);
+
+		rating.find('.descr').hover(function(){
+			stars.removeClass('active');
+			rating_input.val(0);
+		});
+
+		stars.each(function(index){
+			var current = index + 1;
+
+			$(this).hover (
+				function(){
+					stars.removeClass('active').slice(0, current).addClass('active');
+				},
+				function(){
+					rating_input.val(parseFloat($('.active', rating).length/2));
+				}
+			);
+
+		});
+
+	});
+
+	//slider delivery
+	$('.slider__delivery').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		// speed: 600,
+		// centerMode: true,
+		variableWidth: true,
+		autoplay: true,
+		autoplaySpeed: 20000,
+		prevArrow: $('.js-slider__back'),
+		nextArrow: $('.js-slider__next')
+	});
+
+
+
+}); //document ready
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 //=include lib/jquery.js
 //=include lib/slick.min.js
+//=include lib/jquery-ui.min.js
 //=include common.js
